2825cc880b61ef1abf7751bfb123a50eecd9c350
Ahmed Hammad <hammad97official@gmail.com>
2021-06-03 19:12:37 +0200

    Add Cors permissions

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const ip = require('ip');
+const cors = require('cors');
 const routes = require(__dirname + '/routes');
 const websocket = require(__dirname + '/services/socketHandler');
 
 const app = express();
 
-const server = app.listen(8080, ()=>{console.log("Listening at port 8080")});
+const server = app.listen(8080, () => {
+    console.log(`Listening at ${ip.address()} port ${server.address().port}`)
+});
+
+app.use(cors({ origin: '*', credentials: true }));
 
 app.use(routes);
 
