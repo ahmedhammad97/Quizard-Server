22a417404ecbe26e0065947c7a69415730cc97d6
Ahmed Hammad <hammad97official@gmail.com>
2021-06-10 01:57:05 +0200

    Shuffle answers before serving them

--- services/questionHandler.js
+++ services/questionHandler.js
@@ -12,6 +12,9 @@ function shuffleArray(array) {
     for (let i = newArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        newArray.forEach(question => {
+            question.answers.sort( () => .5 - Math.random() )
+        })
     }
     return newArray;
 }
